3871d2202ee9cdba01ebfe50c211aea23a21c6c8
ErwinLima <ealiman@correo.url.edu.gt>
2022-08-30 22:54:09 -0600

    Multiple consults feature

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -38,19 +38,37 @@ public class Program
                         }
                     }                    
                 }
-                Console.WriteLine("Ingrese el nombre de la persona que quiere buscar: ");
-                string? name = Console.ReadLine();
-                Persona temporal = new Persona();
-                temporal.name = name!;
-                arbolPersonas.QueryResults(arbolPersonas.Root,temporal, Delegates.NameComparison, personas);
 
-                string line = "";
-                foreach (var item in personas)
+                string flag = "";
+                do
                 {
-                    line += JsonSerializer.Serialize<Persona>(item) + "\n";
-                }
-                string output = @"C:\Users\AndresLima\Desktop\" + name +".text";
-                File.WriteAllText(output, line);
+                    personas.Clear();
+                    Console.WriteLine("Ingrese el nombre de la persona que quiere buscar: ");
+                    string? name = Console.ReadLine();
+                    Persona temporal = new Persona();
+                    temporal.name = name!;
+                    arbolPersonas.QueryResults(arbolPersonas.Root!, temporal, Delegates.NameComparison, personas);
+                    string line = "";
+                    if (personas.Count() == 0)
+                    {
+                        line = "Sin coincidencias para el nombre " + name;
+                        Console.WriteLine(line);
+                    }
+                    else
+                    {
+                        foreach (var item in personas)
+                        {
+                            line += JsonSerializer.Serialize<Persona>(item) + "\n";
+                        }
+                        string output = @"C:\Users\AndresLima\Desktop\" + name + ".txt";
+                        File.WriteAllText(output, line);
+                    }
+
+                    Console.WriteLine("¿Desea realizar otra búsqueda? Ingrese \'1\' o \'2\' ");
+                    Console.WriteLine("1. Si");
+                    Console.WriteLine("2. No");
+                    flag = Console.ReadLine();
+                } while (flag == "1");                                                
             }
         }
         catch (Exception e)
